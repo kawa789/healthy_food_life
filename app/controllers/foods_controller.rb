69ce736977354df98da4284ba3a9ec4d0d8f84cb
kawa789 <ryou62a@gmail.com>
2022-02-20 08:23:58 +0000

    [Add]投稿削除機能

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -10,27 +10,33 @@ class FoodsController < ApplicationController
   def new
     @food = Food.new
   end
-  
+
   def create
     food = Food.new(food_params)
     food.save
     redirect_to foods_path
   end
-  
+
   def destroy
+    food = Food.find(params[:id])
+    food.destroy
+    redirect_to foods_path
   end
 
   def edit
-    @food = Food.find(params[:])
+    @food = Food.find(params[:id])
   end
-  
+
   def update
+    food = Food.find(params[:id])
+    food.update(food_params)
+    redirect_to foods_path(food)
   end
-  
+
   private
   def food_params
     params.require(:food).permit(:title, :body)
   end
-  
+
 end
 
