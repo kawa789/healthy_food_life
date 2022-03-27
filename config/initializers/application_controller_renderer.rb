1ce310462f74d660aeefbc8cb316cca2d2c9e05b
kawa789 <ryou62a@gmail.com>
2022-03-27 07:53:03 +0000

    [Add]pry-bybug

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '55396051595f59140fdf872b2b55112cf2f082e582dcdea3d71c760f8344f98ec926a6d42e1ba138933423bc1091d0dd11487900ccf3fb61e0e77f071b7a8f0a'
