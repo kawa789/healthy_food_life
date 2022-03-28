5b591a475bed9b4f7d949c8257fb33e2c7aa5800
kawa789 <ryou62a@gmail.com>
2022-03-28 07:49:33 +0000

    [Add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :foods do
+     resource :favorites, only: [:create, :destroy]
     resources :food_comments, only: [:create, :destroy]
   end
 
